0f76741b783e2bf5ddedb69a3c3ff8c47d048f5d
Christopher <Christopher@WINDOWS-L4M9IUT>
2017-09-06 13:49:44 -0400

    First Commit

--- src/pkgMain/Rectangle.java
+++ src/pkgMain/Rectangle.java
@@ -4,6 +4,7 @@ public class Rectangle {
 
 	private double Length;
 	private double Width;
+	private double a;
 	public Rectangle(double length, double width) {
 		Length = length;
 		Width = width;
@@ -17,7 +18,8 @@ public class Rectangle {
 	
 	public double Area()
 	{
-		// TODO Implement Area() function with the correct formula
-		return 0;
+		 a = getLength() * getWidth(); 
+		
+		return a;
 	}	
 }
